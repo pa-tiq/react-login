27ea10fabd9d91243f665d099103404cdadc011f
pa-tiq <patrickdsilva99@gmail.com>
2022-09-19 11:07:23 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <main>
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
