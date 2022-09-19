2e5605b3a54cbef7a3aca9fd7a9dc7e0d0053689
pa-tiq <patrickdsilva99@gmail.com>
2022-09-19 11:09:27 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,31 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoginInfo = localStorage.getItem('isLoggedIn');
+    if(storedUserLoginInfo === '1'){
+      setIsLoggedIn(true);
+    }
+  },[]); //this only runs once - when the app starts
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem('isLoggedIn','1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
 
   return (
     <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
     </React.Fragment>
   );
